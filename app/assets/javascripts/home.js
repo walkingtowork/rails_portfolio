1565a2852adf8d405f2006e16bd80f7589341618
Alex Lin <alexander.lin.kremer@gmail.com>
2013-08-22 16:30:47 -0700

    finished profile page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 
   var textbox = $('#textbox');
 
-  // var project_block = $('.project_block');
-
   var image_thumb = $('.image_thumb');
 
   image_thumb.on('click', function() {
@@ -13,6 +11,34 @@ $(document).ready(function(){
     textbox.append(project.data('name') + ":<br>");
     textbox.append(project.data('message'));
   });
+
+  var about_me_link = $('#about_me_link');
+  var portfolio_link = $('#portfolio_link');
+
+  var status = $('#status');
+  var about_me = $('#about_me');
+  var project_collection = $('#project_collection');
+  var directions = $('#directions');
+
+  about_me_link.on('click', function() {
+    textbox.empty();
+    project_collection.hide();
+    status.show();
+    directions.hide();
+    about_me.show();
+    textbox.append(about_me);
+  });
+
+  portfolio_link.on('click', function() {
+    textbox.empty();
+    about_me.hide();
+    status.hide();
+    project_collection.show();
+    directions.show();
+    textbox.append(directions);
+  });
+
+
 
 
 });
